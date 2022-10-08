ff658549f7e46817df8e53020fef1afc94751236
Geb-algebra <77182026+Geb-algebra@users.noreply.github.com>
2022-10-08 23:38:13 +0900

    initialize project

---
+++ compose/frontend/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+while ! python3 manage.py shell -c "
+from django.db import connection
+with connection.cursor() as cursor:
+    cursor.execute('select 1')
+    cursor.fetchall()
+"
+do
+    echo "Wait until DB is ready..."
+    sleep 5
+done
+
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py runserver 0.0.0.0:8000
